efe2d042ad346af6991b2e5e7a328a586ae536bf
Bao Cong Pham <BaoPham92@yahoo.com>
2017-05-31 14:26:40 -0400

    almost finished edit/delete

--- app.rb
+++ app.rb
@@ -31,3 +31,22 @@ post '/pokemons' do
   @pokemon = Pokemon.create(params[:pokemon])
   redirect "pokemons/index"
 end
+
+get '/pokemons/:id' do
+	@pokemon = Pokemon.find(params[:id])
+	erb :"pokemons/show"
+end
+
+get "/pokemons/:id/edit" do 
+	@pokemon = Pokemon.find(params[:id])
+	erb(:"pokemons/edit")
+end
+
+put '/pokemons/:id' do 
+	@pokemon = pokemon.find(params[:id])
+	@pokemon.update(params[:id])
+	redirect("/pokemons/#{@pokemon.id}")
+end
+
+
+
